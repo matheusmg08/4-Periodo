7acb4da76d896dbbad11c4ee96b05294e6275b63
Matheus <129191137+matheusmg08@users.noreply.github.com>
2024-09-26 20:35:59 -0300

    Add files via upload

---
+++ Desenvolvimento de Sistemas Web-Java 1/3º Bimestre/Projeto Trabalho/Trabalhobean/Java/org.trabalho/Pessoa2.java
@@ -0,0 +1,95 @@
+package org.trabalho;
+
+public class Pessoa2 {
+	private int idvendedor;
+	private String nome;
+	private String telefone;
+	private String email;
+	private String cidade;
+	
+	
+	
+	
+	public int getIdvendedor() {
+		return idvendedor;
+	}
+
+
+
+
+
+	public void setIdvendedor(int idvendedor) {
+		this.idvendedor = idvendedor;
+	}
+
+
+
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+
+	public String getCidade() {
+		return cidade;
+	}
+
+
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+
+
+
+	public String toString() {
+		return "";
+	}
+	
+		
+
+}
+
